661622db70fdd3d690cc6dfcf2435b45b98e9ede
Pedroliaan <99298485+Pedroliaan@users.noreply.github.com>
2023-11-10 14:27:41 -0300

    Add files via upload

---
+++ projeto final/classes/carro.py
@@ -0,0 +1,30 @@
+class Carro:
+    def __init__(self, marca:str, modelo:str, ano:int, cor:str, preco:float):
+        
+
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__ano = ano
+        self.__cor = cor
+        self.__preco = preco
+        
+    def get_marca(self):
+        return self.__marca
+    def set_marca(self, marca):
+        self.__marca = marca
+    def get_modelo(self):
+        return self.__modelo
+    def set_modelo(self, modelo):
+        self.__modelo = modelo
+    def get_ano(self):
+        return self.__ano
+    def set_ano(self, ano):
+        self.__ano = ano
+    def get_cor(self):
+        return self.__cor
+    def set_cor(self, cor):
+        self.__cor = cor
+    def get_preco(self):
+        return self.__preco
+    def set_preco(self, preco):
+        self.__preco = preco
---
+++ projeto final/utils.py
@@ -0,0 +1,10 @@
+from classes.arquivo import Arquivo
+from classes.carro import Carro
+
+def registrarcarro():
+    
+
+
+
+
+
